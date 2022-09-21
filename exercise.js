5b690c6a1a2ff54499b18943708b943efde058d0
SarinaSaffarpour <108338896+SarinaSaffarpour@users.noreply.github.com>
2022-09-21 05:21:58 +0300

    Js Es 6

---
+++ exercise.js
@@ -0,0 +1,7 @@
+let hello = "Ciao"; // boolean
+let age = 18; // string
+let isGraduated = false; // number
+let booleanOfHello = Boolean(hello);
+let stringOfAge = String(age);
+let numberOfIsGraduated = Number(isGraduated);
+console.log(booleanOfHello + "   " + stringOfAge + "  " + numberOfIsGraduated);
